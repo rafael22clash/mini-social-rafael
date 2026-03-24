e1a348ba7dd3c61acec88bdc822081c9736a0e59
rafael22clash <rafael.9623560@aluno.mg.gov.br>
2026-03-24 17:25:38 +0000

    mudança botão

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 let likeCount = 0;
+let curtido = false; // flag booleana
 
 function curtir() {
-  likeCount++;
-  document.getElementById("likeCount").innerText = likeCount;
+
+ if(curtido == false){
+    likeCount++;
+    curtido = true;
+    document.getElementById("likeCount").innerText = likeCount;
+ }else{
+    likeCount--;
+    curtido = false;
+    document.getElementById("likeCount").innerText = likeCount;
+ }
+
+  
 }
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
