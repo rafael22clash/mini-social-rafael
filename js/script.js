14226465d39020141802bb8f4dce4c934680cd13
rafael22clash <rafael.9623560@aluno.mg.gov.br>
2026-03-24 18:22:45 +0000

    botão de deslike

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 let likeCount = 0;
+let desCount = 0;
+
 let curtido = false; // flag booleana
+let descurtido = false;
 
 function curtir() {
 
@@ -12,8 +15,22 @@ function curtir() {
     curtido = false;
     document.getElementById("likeCount").innerText = likeCount;
  }
+}
 
+
+
+ function deslike () {
+if(descurtido == false){
+    desCount++;
+    descurtido = true;
+    document.getElementById("desCount").innerText = desCount;
+ }else{
+    desCount--;
+    descurtido = false;
+    document.getElementById("desCount").innerText = desCount;
+ }
   
 }
 
 document.getElementById("likeBtn").addEventListener("click", curtir);
+document.getElementById("desBtn").addEventListener("click", deslike);
